c628db6a6487b9d6fcc068cd93209ec9cbcfd9c0
Sawood Alam <ibnesayeed@gmail.com>
2017-11-28 22:59:43 -0500

    Step 2

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# Link Extractor: Step 1
+# Link Extractor: Step 2
 
-A basic page scraping script that prints out all the hyper references of the given web page.
+A basic page scraping script that prints out all the hyper references and anchor texts of the given web page.
 
 ## Changes from the previous step
 
-* Adds a `Dockerfile` to fix various issues describe in the previous step
+* Paths are normalized to full URLs
+* Reporting both links and anchor texts
+* Usable as a module in other scripts
 
 ## Try it out
 
 ```
-$ docker image build -t linkextractor:step1 .
-$ docker container run -it --rm linkextractor:step1 http://example.com/
-$ docker container run -it --rm linkextractor:step1 http://odu.edu/
+$ docker image build -t linkextractor:step2 .
+$ docker container run -it --rm linkextractor:step2 http://example.com/
+$ docker container run -it --rm linkextractor:step2 http://odu.edu/
 ```
--- linkextractor.py
+++ linkextractor.py
@@ -3,8 +3,24 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-res = requests.get(sys.argv[-1])
-soup = BeautifulSoup(res.text, "html.parser")
-for link in soup.find_all("a"):
-    print(link.get("href"))
+def extract_links(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "html.parser")
+    base = url
+    # TODO: Update base if a <base> element is present with the href attribute
+    links = []
+    for link in soup.find_all("a"):
+        links.append({
+            "text": " ".join(link.text.split()) or "[IMG]",
+            "href": urljoin(base, link.get("href"))
+        })
+    return links
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("\nUsage:\n\t{} <URL>\n".format(sys.argv[0]))
+        sys.exit(1)
+    for link in extract_links(sys.argv[-1]):
+        print("[{}]({})".format(link["text"], link["href"]))
